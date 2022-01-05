f83e59ef541cfd53c32e31edc1f010667bf09af0
한광재 <hkj4900@pusan.ac.kr>
2022-01-05 13:19:11 +0900

    Add: RotatingString 컴포넌트 Home.vue에 추가

--- mission/src/views/Home.vue
+++ mission/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RotateString />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import RotateString from '@/components/RotateString.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    RotateString,
   },
 };
 </script>
