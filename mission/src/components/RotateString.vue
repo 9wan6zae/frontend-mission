8bc142b0165ebdaa60a840340fdbc8278df1b0b0
한광재 <hkj4900@pusan.ac.kr>
2022-01-05 13:19:34 +0900

    Feat: 문자열 회전 기능 구현

---
+++ mission/src/components/RotateString.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type='text' v-model="string" />
+    <button @click="rotate">회전</button>
+    <p>{{ string }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+const useRotate = (propsString = '') => {
+  const string = ref(`${propsString}`);
+  const rotate = () => {
+    string.value = string.value.slice(1) + string.value.slice(0, 1);
+  };
+  return { string, rotate };
+};
+
+export default defineComponent({
+  name: 'RotateString',
+  props: {
+    propsString: String,
+  },
+  setup(props) {
+    const { string, rotate } = useRotate(props.propsString);
+    return {
+      string,
+      rotate,
+    };
+  },
+});
+</script>
