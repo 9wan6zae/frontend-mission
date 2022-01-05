22fdbcad1781c406648098a0a554335ff1df2b13
한광재 <hkj4900@pusan.ac.kr>
2022-01-05 12:54:02 +0900

    Test: 문자열 회전 테스트 코드 추가

---
+++ mission/tests/unit/rotateString.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount } from '@vue/test-utils';
+import RotateString from '@/components/RotateString.vue';
+
+describe('RotateString.vue', () => {
+  test('문자열 입력', async () => {
+    const propsString = 'Projectlion';
+    const wrapper = shallowMount(RotateString, {
+      props: { propsString }
+    });
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(wrapper.text()).toContain('rojectlionP')
+  });
+  test('숫자 입력', async () => {
+    const propsString = 12345;
+    const wrapper = shallowMount(RotateString, {
+      props: { propsString }
+    });
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(wrapper.text()).toContain('23451')
+  });
+  test('반복 클릭', async () => {
+    const propsString = 'abcde';
+    const wrapper = shallowMount(RotateString, {
+      props: { propsString }
+    });
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    await button.trigger('click')
+    await button.trigger('click')
+    await button.trigger('click')
+    await button.trigger('click')
+    expect(wrapper.text()).toContain('abcde')
+  });
+});
