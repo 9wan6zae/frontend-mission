7ac9cf2c015deedf0c4c1576c8fd75105ff77b89
한광재 <hkj4900@pusan.ac.kr>
2022-01-05 14:52:33 +0900

    Feat: 모달 컴포넌트 구현

---
+++ mission/src/components/AlertModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <Modal ref="modal">
+    <p v-for="text in content" :key="text">
+      {{ text }}
+    </p>
+    <footer>
+      <button class="close-btn" @click="cancel">닫기</button>
+    </footer>
+  </Modal>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import Modal from '@/teleport/Modal.vue';
+
+export default defineComponent({
+  name: 'AlertModal',
+  components: {
+    Modal,
+  },
+  props: {
+    content: Array,
+  },
+  setup() {
+    const modal = ref(null);
+
+    const show = () => {
+      modal.value.open();
+    };
+
+    const cancel = () => {
+      modal.value.close();
+    };
+    return {
+      modal, show, cancel,
+    };
+  },
+});
+</script>
---
+++ mission/src/teleport/Modal.vue
@@ -0,0 +1,73 @@
+<template>
+  <teleport to='body'>
+    <div class="modal" v-show="isShow" @click="isShow = false">
+      <div class='modal__container'>
+        <slot />
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const isShow = ref(false);
+
+    const open = () => {
+      isShow.value = true;
+    };
+
+    const close = () => {
+      isShow.value = false;
+    };
+
+    return { isShow, open, close };
+  },
+});
+</script>
+
+<style>
+.modal {
+  z-index: 999;
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.4);
+}
+.modal__container {
+  position: relative;
+  width: 320px;
+  height: 200px;
+  padding: 10px;
+  box-sizing: border-box;
+  background: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.16);
+}
+.modal__container > footer {
+  position: absolute;
+  left: 10px;
+  right: 10px;
+  bottom: 10px;
+  display: flex;
+  justify-content: end;
+  box-sizing: border-box;
+}
+button.close-btn {
+  background-color: #ff5160;
+  border-radius: 10px;
+  width: 80px;
+  height: 40px;
+  border: none;
+  outline: none;
+  color: white;
+  box-shadow: 0 2px 8px 0 rgb(0 0 0 / 20%);
+}
+</style>
