6aaacfc4b8da9f81d291a3131cb2851def36841f
한광재 <hkj4900@pusan.ac.kr>
2022-01-05 14:19:01 +0900

    Style: Eslint 적용 Semicolon 누락, Comma 누락

--- mission/tests/unit/rotateString.spec.js
+++ mission/tests/unit/rotateString.spec.js
@@ -5,32 +5,32 @@ describe('RotateString.vue', () => {
   test('문자열 입력', async () => {
     const propsString = 'Projectlion';
     const wrapper = shallowMount(RotateString, {
-      props: { propsString }
+      props: { propsString },
     });
-    const button = wrapper.find('button')
-    await button.trigger('click')
-    expect(wrapper.text()).toContain('rojectlionP')
+    const button = wrapper.find('button');
+    await button.trigger('click');
+    expect(wrapper.text()).toContain('rojectlionP');
   });
   test('숫자 입력', async () => {
     const propsString = 12345;
     const wrapper = shallowMount(RotateString, {
-      props: { propsString }
+      props: { propsString },
     });
-    const button = wrapper.find('button')
-    await button.trigger('click')
-    expect(wrapper.text()).toContain('23451')
+    const button = wrapper.find('button');
+    await button.trigger('click');
+    expect(wrapper.text()).toContain('23451');
   });
   test('반복 클릭', async () => {
     const propsString = 'abcde';
     const wrapper = shallowMount(RotateString, {
-      props: { propsString }
+      props: { propsString },
     });
-    const button = wrapper.find('button')
-    await button.trigger('click')
-    await button.trigger('click')
-    await button.trigger('click')
-    await button.trigger('click')
-    await button.trigger('click')
-    expect(wrapper.text()).toContain('abcde')
+    const button = wrapper.find('button');
+    await button.trigger('click');
+    await button.trigger('click');
+    await button.trigger('click');
+    await button.trigger('click');
+    await button.trigger('click');
+    expect(wrapper.text()).toContain('abcde');
   });
 });
