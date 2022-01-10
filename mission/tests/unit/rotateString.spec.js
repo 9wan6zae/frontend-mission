69ecb64b01b7e8884dae1100622aa807b68c4ac9
한광재 <hkj4900@pusan.ac.kr>
2022-01-10 17:49:12 +0900

    Fix: TC 수정
    - Arrange, Assert, Act 단계로 구분되도록 분리
    
    - 가능한 한 vm 객체를 통해 직접 접근하는 코드 지우기
    하지만, teleport의 경우 예제가 별로 없어 어려움을 느낌
    -> 확인 후 수정 예정

--- mission/tests/unit/rotateString.spec.js
+++ mission/tests/unit/rotateString.spec.js
@@ -2,40 +2,43 @@ import { shallowMount, mount } from '@vue/test-utils';
 import RotateString from '@/components/RotateString.vue';
 
 describe('RotateString.vue', () => {
-  test('문자열 입력', async () => {
-    const propsString = 'Projectlion';
-    const wrapper = shallowMount(RotateString, {
-      props: { propsString },
-    });
+  test('버튼 클릭시 문자열 회전되는지 확인한다.', async () => {
+    const wrapper = shallowMount(RotateString);
+
+    await wrapper.get('[data-test="input"]').setValue('Projectlion');
+
     const button = wrapper.find('button');
     await button.trigger('click');
+
     expect(wrapper.text()).toContain('rojectlionP');
   });
-  test('반복 클릭', async () => {
-    const propsString = 'abcde';
-    const wrapper = shallowMount(RotateString, {
-      props: { propsString },
-    });
-    const button = wrapper.find('button');
-    await button.trigger('click');
-    await button.trigger('click');
-    await button.trigger('click');
-    await button.trigger('click');
-    await button.trigger('click');
+  test('반복 클릭시 정상적으로 동작하는지 확인한다.', async () => {
+    const wrapper = shallowMount(RotateString);
+
+    await wrapper.get('[data-test="input"]').setValue('abcde');
+
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    await wrapper.get('[data-test="submit"]').trigger('click');
+
     expect(wrapper.text()).toContain('abcde');
   });
-  test('모달 값 유효성 테스트', async () => {
-    const propsString = 'abcde';
-    const wrapper = mount(RotateString, {
-      props: { propsString },
-    });
+  test('모달에 원하는 값이 표시되는지 확인한다.', async () => {
+    const wrapper = mount(RotateString);
     const { vm } = wrapper;
-    vm.rotate();
-    vm.rotate();
-    vm.modalShow();
-    vm.modalShow();
-    vm.modalShow();
-    vm.modalShow();
+
+    await wrapper.get('[data-test="input"]').setValue('abcde');
+    // 제출 버튼 2번 클릭 => abcde -> cdeab
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    await wrapper.get('[data-test="submit"]').trigger('click');
+    // 알림 버튼 4번 클릭 => 버튼을 누른 횟수가 4로 됨
+    await wrapper.get('[data-test="alert"]').trigger('click');
+    await wrapper.get('[data-test="alert"]').trigger('click');
+    await wrapper.get('[data-test="alert"]').trigger('click');
+    await wrapper.get('[data-test="alert"]').trigger('click');
+
     expect(vm.modalContent).toEqual([
       '문자열: cdeab',
       '버튼을 누른 횟수: 4',
