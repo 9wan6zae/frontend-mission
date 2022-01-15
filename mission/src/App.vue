8249999b5fc2f73a02916734d563518c376516f7
한광재 <hkj4900@pusan.ac.kr>
2022-01-15 17:07:14 +0900

    Style: 사용하지 않는 프로퍼티 제거, 커스텀 클래스 추가

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -3,11 +3,48 @@
 </template>
 
 <style>
+:root {
+  --black: #1f1f1f;
+  --darkgray: #9E9E9E;
+  --lightgray: #ddd;
+  --emphasis: #FF5160;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+html, body {
+  margin: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.box-shadow {
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.pa0-20 {
+  padding: 0 20px;
+}
+
+.flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.flex-justify-center {
+  display: flex;
+  justify-content: center;
+}
+
+.flex-align-center {
+  display: flex;
+  align-items: center;
 }
 </style>
