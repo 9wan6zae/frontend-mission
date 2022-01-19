9f6b08f5f1401ea68ebbe90465a03de2e44c69bb
한광재 <hkj4900@pusan.ac.kr>
2022-01-19 20:09:10 +0900

    Feat: Item TC를 통과할 정도의 마크업 구현

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="item-list-item"></div>
+  <div class="item-list-item">
+    <img :src="item?.img" data-test="item-img" />
+    <p v-if="item?.discountRate" data-test="discount-rate">{{ item?.discountRate }}%</p>
+    <p data-test="price">{{ salesPrice }}</p>
+    <p data-test="item-name">{{ item?.name }}</p>
+    <p data-test="item-description">{{ item?.description }}</p>
+  </div>
 </template>
 
 <script>
+import comma from '@/mixins/comma';
+
 export default {
   name: 'ItemListItem',
+  mixins: [comma],
+  props: {
+    item: Object,
+  },
+  computed: {
+    salesPrice() {
+      if (this.item?.discountRate) {
+        return `${this.addComma(Math.ceil(this.item?.originalPrice * ((1 - (this.item?.discountRate / 100)))))}원`;
+      }
+      return `${this.addComma(this.item?.originalPrice)}원`;
+    },
+  },
 };
 </script>
 
