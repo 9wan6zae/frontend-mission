dd2c75a12e52cbadaf120a05b9f9c6f5aef697b0
한광재 <hkj4900@pusan.ac.kr>
2022-01-19 21:31:12 +0900

    Feat: NavBar TC를 통과할 정도의 마크업 구현

---
+++ mission/src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="nav-bar">
+    <nav data-test="nav-menu-bar">
+      <div v-for="menu in navMenu" :key="menu.title" data-test="nav-menu">
+        <font-awesome-icon :icon="menu.icon" data-test="nav-menu-icon" />
+        <p data-test="nav-menu-title">{{ menu.title }}</p>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navMenu: [
+        {
+          icon: ['fas', 'star'],
+          title: '홈',
+        },
+        {
+          icon: ['fas', 'car'],
+          title: '찜',
+        },
+        {
+          icon: ['fas', 'user'],
+          title: '장바구니',
+        },
+        {
+          icon: ['fas', 'star'],
+          title: '마이페이지',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
