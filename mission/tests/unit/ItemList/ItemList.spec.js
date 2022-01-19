b0c7b783d99b77a55c3137199415151584ec807c
한광재 <hkj4900@pusan.ac.kr>
2022-01-19 16:14:20 +0900

    Test: ItemList 관련 TC 작성
    - item-list-wrapper가 렌더링 되었는지
    - N개의 아이템이 있으면 N개를 렌더링 하는지

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -7,4 +7,48 @@ describe('ItemListPage', () => {
 
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
   });
+
+  it('renders item-list-wrapper', () => {
+    const wrapper = mount(ItemListPage);
+
+    expect(wrapper.find('[data-test="item-list-wrapper"]').exists).toBeTruthy();
+  });
+
+  it('renders N when there are N item', () => {
+    const itemList = [
+      {
+        id: 1,
+        img: 'test-img',
+        discountRate: 15,
+        originalPrice: 10000,
+        name: 'test-name',
+        description: 'test-description',
+      },
+      {
+        id: 2,
+        img: 'test-img',
+        discountRate: 15,
+        originalPrice: 10000,
+        name: 'test-name',
+        description: 'test-description',
+      },
+      {
+        id: 3,
+        img: 'test-img',
+        discountRate: 15,
+        originalPrice: 10000,
+        name: 'test-name',
+        description: 'test-description',
+      },
+    ];
+    const wrapper = mount(ItemListPage, {
+      data() {
+        return {
+          itemList,
+        };
+      },
+    });
+
+    expect(wrapper.findAll('[data-test="item-list"]').length).toEqual(itemList.length);
+  });
 });
