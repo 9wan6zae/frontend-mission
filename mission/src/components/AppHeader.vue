82a56e928bfd80daf581575089116dc546988982
한광재 <hkj4900@pusan.ac.kr>
2022-01-19 20:38:37 +0900

    Feat: AppHeader TC를 통과할 정도의 마크업 구현

---
+++ mission/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app-header">
+    <p data-test="header-title">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '9wanZae\'s shop',
+      scollY: 0,
+    };
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
