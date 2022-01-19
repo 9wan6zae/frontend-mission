1ce4eb260e1dd248299fc1fdc78946bd375d5da7
한광재 <hkj4900@pusan.ac.kr>
2022-01-19 16:27:51 +0900

    Feat: ItemList 마크업

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,10 +1,43 @@
 <template>
-  <div id="item-list-page"></div>
+  <div id="item-list-page">
+    <section data-test="item-list-wrapper">
+      <article v-for="item in itemList" :key="item.id" data-test="item-list">
+        <ItemListItem :item="item" />
+      </article>
+    </section>
+  </div>
 </template>
 
 <script>
+import ItemListItem from '@/components/ItemList/Item.vue';
+
 export default {
   name: 'ItemListPage',
+  components: {
+    ItemListItem,
+  },
+  data() {
+    return {
+      itemList: [
+        {
+          id: 1,
+          img: 'test-img',
+          discountRate: 15,
+          originalPrice: 10000,
+          name: 'test-name',
+          description: 'test-description',
+        },
+        {
+          id: 2,
+          img: 'test-img',
+          discountRate: 15,
+          originalPrice: 10000,
+          name: 'test-name2',
+          description: 'test-description',
+        },
+      ],
+    };
+  },
 };
 </script>
 
