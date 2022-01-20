7e921cd42b52eee890d025681cd558670f5615a4
한광재 <hkj4900@pusan.ac.kr>
2022-01-20 17:08:01 +0900

    Feat: Item 컴포넌트 구현

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="item-list-item">
-    <img :src="item?.img" data-test="item-img" />
-    <p v-if="item?.discountRate" data-test="discount-rate">{{ item?.discountRate }}%</p>
-    <p data-test="price">{{ salesPrice }}</p>
-    <p data-test="item-name">{{ item?.name }}</p>
-    <p data-test="item-description">{{ item?.description }}</p>
+    <figure>
+      <img :src="item?.img" data-test="item-img" />
+    </figure>
+    <section class="price-section">
+      <span
+        v-if="isDiscount"
+        class="discount-rate"
+        data-test="discount-rate"
+      >
+        {{ item?.discountRate }}%
+      </span>
+      <span
+        class="sales-price"
+        data-test="price"
+      >
+        {{ salesPrice }}
+      </span>
+    </section>
+    <p
+      class="item-name one-line-ellipsis"
+      data-test="item-name"
+    >
+      {{ item?.name }}
+    </p>
+    <p
+      class="item-description one-line-ellipsis"
+      data-test="item-description"
+    >
+      {{ item?.description }}
+    </p>
   </div>
 </template>
 
@@ -18,6 +43,9 @@ export default {
     item: Object,
   },
   computed: {
+    isDiscount() {
+      return this.item?.discountRate;
+    },
     salesPrice() {
       if (this.item?.discountRate) {
         return `${this.addComma(Math.ceil(this.item?.originalPrice * ((1 - (this.item?.discountRate / 100)))))}원`;
@@ -28,5 +56,61 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.item-list-item {
+  width: 100%;
+  height: 250px;
+}
+
+.item-list-item figure {
+  margin: 0;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 100%;
+  background: #ddd;
+  border-radius: 10px;
+}
+
+.item-list-item figure img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.price-section {
+  margin-top: 6px;
+  margin-bottom: 4px;
+}
+
+.discount-rate {
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--emphasis);
+}
+
+.sales-price {
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.one-line-ellipsis {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.item-name {
+  font-weight: 600;
+  font-size: 14px;
+}
+
+.item-description {
+  font-weight: 400;
+  font-size: 14px;
+}
 </style>
