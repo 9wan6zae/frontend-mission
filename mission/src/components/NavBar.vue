12c69a96d13d16e33ec4214414bf0c3877dc9476
한광재 <hkj4900@pusan.ac.kr>
2022-01-20 14:36:01 +0900

    Feat: NavBar 하단 고정 기능 구현

--- mission/src/components/NavBar.vue
+++ mission/src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="nav-bar">
-    <nav data-test="nav-menu-bar">
-      <div v-for="menu in navMenu" :key="menu.title" data-test="nav-menu">
+    <nav data-test="nav-menu-bar" class="flex-space-between">
+      <article v-for="menu in navMenu" :key="menu.title" data-test="nav-menu">
         <font-awesome-icon :icon="menu.icon" data-test="nav-menu-icon" />
-        <p data-test="nav-menu-title">{{ menu.title }}</p>
-      </div>
+        <p class="menu-title" data-test="nav-menu-title">{{ menu.title }}</p>
+      </article>
     </nav>
   </div>
 </template>
@@ -37,5 +37,28 @@ export default {
 </script>
 
 <style scoped>
+#nav-bar {
+  width: 100%;
+  height: var(--navBarHeight);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
 
+#nav-bar nav {
+  border-top: 1px solid var(--darkgray);
+  text-align: center;
+  padding: 10px 20px 20px 20px;
+}
+
+#nav-bar nav article{
+  width: 80px;
+}
+
+#nav-bar nav article .menu-title{
+  margin-top: 2px;
+  font-size: 14px;
+  font-weight: 400;
+}
 </style>
