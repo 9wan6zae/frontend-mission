38dfbbf95c9daa364de5930bf2b061ed9b55e0f0
한광재 <hkj4900@pusan.ac.kr>
2022-01-20 14:25:54 +0900

    Feat: 스크롤 시 헤더가 사라지는 기능 구현

--- mission/src/components/AppHeader.vue
+++ mission/src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app-header">
-    <p data-test="header-title">{{ title }}</p>
+  <div :class="appHeaderClass" id="app-header">
+    <h1 data-test="header-title">{{ title }}</h1>
   </div>
 </template>
 
@@ -8,16 +8,58 @@
 export default {
   data() {
     return {
-      title: '9wanZae\'s shop',
-      scollY: 0,
+      title: '9wan6zae\'s Shop',
+      scrollY: 0,
     };
   },
-  mounted() {
-
+  methods: {
+    setScrollEvent() {
+      let timer;
+      window.addEventListener('scroll', () => {
+        if (!timer) {
+          timer = setTimeout(() => {
+            timer = null;
+            this.scrollY = window.scrollY;
+          }, 100);
+        }
+      });
+    },
+  },
+  computed: {
+    appHeaderClass() {
+      return this.scrollY === 0 ? 'app-header--visible' : 'app-header--hidden';
+    },
+  },
+  created() {
+    this.setScrollEvent();
   },
 };
 </script>
 
 <style scoped>
+#app-header {
+  width: 100%;
+  height: var(--appHeaderHeight);
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: transform .2s ease-out;
+}
 
+#app-header h1 {
+  font-size: 24px;
+  font-weight: 600;
+  color: var(--black);
+  text-align: center;
+}
+
+.app-header--visible {
+  transform: translateY(0);
+}
+
+.app-header--hidden {
+  transform: translateY(-100%);
+}
 </style>
