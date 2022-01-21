00ee2f16fe5c7c96042e01849c3961e8ef1e3c02
한광재 <hkj4900@pusan.ac.kr>
2022-01-21 14:37:47 +0900

    Test: isDiscount 관련 TC 추가

--- mission/tests/unit/composables/usePrice.spec.js
+++ mission/tests/unit/composables/usePrice.spec.js
@@ -10,4 +10,19 @@ describe('usePrice', () => {
     const { salesPrice } = usePrice(58000);
     expect(salesPrice.value).toBe('58,000원');
   });
+
+  it('makes isDiscount true when there is a discount-rate', () => {
+    const { isDiscount } = usePrice(58000, 1);
+    expect(isDiscount.value).toBeTruthy();
+  });
+
+  it('makes isDiscount false when there is no a discount-rate', () => {
+    const { isDiscount } = usePrice(58000);
+    expect(isDiscount.value).toBeFalsy();
+  });
+
+  it('makes isDiscount false when discount-rate is 0', () => {
+    const { isDiscount } = usePrice(58000, 0);
+    expect(isDiscount.value).toBeFalsy();
+  });
 });
