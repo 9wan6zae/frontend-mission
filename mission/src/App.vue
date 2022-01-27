c3bd1ddf0caa11037d3092a685a367633da308fd
한광재 <hkj4900@pusan.ac.kr>
2022-01-27 16:19:09 +0900

    Refactor: App.vue에 있는 코드 외부 파일로 import
    reset.css: 브라우저 기본 css 대신에 사용하는 css
    
    util.css: 주로 사용하는 css 정의

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -3,60 +3,6 @@
 </template>
 
 <style>
-:root {
-  --black: #1f1f1f;
-  --darkgray: #9E9E9E;
-  --lightgray: #ddd;
-  --emphasis: #FF5160;
-  --appHeaderHeight: 60px;
-  --navBarHeight: 80px;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-html, body {
-  margin: 0;
-}
-
-p, span {
-  margin: 0;
-  color: var(--black);
-}
-
-* {
-  box-sizing: border-box;
-}
-
-.box-shadow {
-  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
-}
-
-.pa0-20 {
-  padding: 0 20px;
-}
-
-.flex-center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.flex-justify-center {
-  display: flex;
-  justify-content: center;
-}
-
-.flex-space-between {
-  display: flex;
-  justify-content: space-between;
-}
-
-.flex-align-center {
-  display: flex;
-  align-items: center;
-}
+@import url('./css/reset.css');
+@import url('./css/util.css');
 </style>
