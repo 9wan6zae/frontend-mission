01fc2fda9afa78634e66abf3cc2e1c101e93d032
한광재 <hkj4900@pusan.ac.kr>
2022-02-02 13:36:25 +0900

    Feat: Nav 메뉴 버튼 클릭시 라우팅 기능 추가

--- mission/src/components/NavBar.vue
+++ mission/src/components/NavBar.vue
@@ -1,8 +1,19 @@
 <template>
     <nav id="nav-bar" data-test="nav-menu-bar">
       <div class="nav-menu-wrapper flex-space-between">
-        <article v-for="menu in navMenu" :key="menu.title" data-test="nav-menu">
-          <font-awesome-icon :icon="menu.icon" data-test="nav-menu-icon" />
+        <article
+          v-for="menu in navMenu"
+          :key="menu.title"
+          data-test="nav-menu"
+          @click="redirect(menu.path)"
+        >
+          <font-awesome-icon
+            :icon="menu.icon"
+            data-test="nav-menu-icon"
+            :style="{
+              color: setIconColor(menu.path)
+            }"
+          />
           <p class="menu-title" data-test="nav-menu-title">{{ menu.title }}</p>
         </article>
       </div>
@@ -17,21 +28,33 @@ export default {
         {
           icon: ['fas', 'home'],
           title: '홈',
+          path: '/',
         },
         {
           icon: ['fas', 'heart'],
           title: '찜',
+          path: '/wish',
         },
         {
           icon: ['fas', 'shopping-cart'],
           title: '장바구니',
+          path: '/cart',
         },
         {
           icon: ['fas', 'user'],
           title: '마이페이지',
+          path: '/mypage',
         },
       ],
     };
+  },
+  methods: {
+    redirect(path) {
+      this.$router.push(path);
+    },
+    setIconColor(path) {
+      return this.$route?.path === path ? 'var(--emphasis)' : 'var(--black)';
+    },
   },
 };
 </script>
