fe0fd4b6663d937749fc7292e719d59fca106f6d
한광재 <hkj4900@pusan.ac.kr>
2022-02-02 11:07:13 +0900

    Feat: Layout 컴포넌트 구현

---
+++ mission/src/components/Layout.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="layout">
+    <AppHeader :menuName="menuName" />
+    <font-awesome-icon
+      @click="prevHistory"
+      v-if="prev"
+      id="prev-btn"
+      size="lg"
+      :icon="['fas', 'chevron-circle-left']"
+      data-test="prev-btn"
+    />
+    <main data-test="main-content">
+      <slot></slot>
+    </main>
+    <NavBar v-if="nav" />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'Layout',
+  components: {
+    AppHeader,
+    NavBar,
+  },
+  props: {
+    prev: {
+      type: Boolean,
+      default: false,
+    },
+    nav: {
+      type: Boolean,
+      default: true,
+    },
+    menuName: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    prevHistory() {
+      window.history.back();
+    },
+  },
+};
+</script>
+
+<style scoped>
+#prev-btn {
+  z-index: 999;
+  position: fixed;
+  left: 10px;
+  top: 32px;
+}
+
+main {
+  padding-top: var(--appHeaderHeight);
+  padding-bottom: var(--navBarHeight);
+  width: 100%;
+}
+</style>
