5f9bf933d3ca80687f740ee8f3684b479bb59d14
한광재 <hkj4900@pusan.ac.kr>
2022-02-03 12:41:14 +0900

    Test: WishList 컴포넌트 관련 TC 추가

---
+++ mission/tests/unit/WishList/WishList.spec.js
@@ -0,0 +1,70 @@
+import { mount } from '@vue/test-utils';
+import WishPage from '@/views/WishList.vue';
+import wishAPI from '@/api/wishAPI';
+
+describe('WishPage', () => {
+  it('redners WishPage', () => {
+    const wrapper = mount(WishPage);
+
+    expect(wrapper.find('#item-list-page').exists()).toBe(true);
+  });
+
+  it('renders item-list-wrapper', () => {
+    const wrapper = mount(WishPage);
+
+    expect(wrapper.find('[data-test="item-list-wrapper"]').exists).toBeTruthy();
+  });
+
+  it('renders N when there are N item', () => {
+    const itemsLength = 3;
+    const items = Array(itemsLength).fill({
+      product_no: 'asdf1234',
+      name: '핏이 좋은 수트',
+      image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+      price: 198000.0,
+      original_price: 298000.0,
+      description: '아주 잘 맞는 수트',
+    });
+    const wrapper = mount(WishPage, {
+      data() {
+        return {
+          items,
+        };
+      },
+    });
+
+    expect(wrapper.findAll('[data-test="item"]').length).toEqual(itemsLength);
+  });
+
+  describe('ItemAPI', () => {
+    const items = Array(3).fill({
+      product_no: 'asdf1234',
+      name: '핏이 좋은 수트',
+      image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+      price: 198000,
+      original_price: 298000,
+      description: '아주 잘 맞는 수트',
+    });
+    wishAPI.get = jest.fn().mockResolvedValue({
+      data: {
+        items,
+      },
+    });
+    const wrapper = mount(WishPage);
+
+    it('wishAPI 호출하는지', (done) => {
+      wrapper.vm.$nextTick(async () => {
+        expect(wishAPI.get).toHaveBeenCalled();
+        done();
+      });
+    });
+    it('itemAPI를 통해 받은 items만큼 렌더링되는지', async (done) => {
+      await wrapper.vm.$nextTick(async () => {
+        expect(wishAPI.get).toHaveBeenCalled();
+        await wishAPI.get();
+        expect(wrapper.findAll('[data-test="item"]').length).toEqual(items.length);
+        done();
+      });
+    });
+  });
+});
