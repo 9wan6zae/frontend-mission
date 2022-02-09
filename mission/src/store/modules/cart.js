4024456ed70503f1596c9a6976821346fe42f0a1
한광재 <hkj4900@pusan.ac.kr>
2022-02-09 16:53:14 +0900

    Feat: cart vuex 등록

---
+++ mission/src/store/modules/cart.js
@@ -0,0 +1,40 @@
+const cart = {
+  namespaced: true,
+  state: {
+    items: [
+      {
+        product_no: 'asdf1234',
+        is_check: false,
+        quantity: 1,
+        name: '핏이 좋은 수트',
+        image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
+        price: 197000,
+        original_price: 298000,
+        description: '아주 잘 맞는 수트',
+      },
+    ],
+  },
+  getters: {
+    totalPrice: (state) => state.items
+      .reduce((acc, cur) => (cur.is_check ? acc + cur.price * cur.quantity : acc), 0),
+  },
+  mutations: {
+    changeQuantity: (state, { index, quantity }) => {
+      state.items[index].quantity = quantity;
+    },
+    toggleIsCheck: (state, index) => {
+      state.items[index].is_check = !state.items[index].is_check;
+    },
+    allCheck: (state, boolean) => {
+      for (let i = 0; i < state.items.length; i += 1) {
+        const item = state.items[i];
+        item.is_check = !boolean;
+      }
+    },
+  },
+  actions: {
+
+  },
+};
+
+export default cart;
