0e234912fff533019572fdcd1ee25eb5f7778c8a
한광재 <hkj4900@pusan.ac.kr>
2022-02-10 16:40:20 +0900

    Feat: 주문하기 페이지에 보여줄 주문내역 요약 정보 구현

--- mission/src/store/modules/cart.js
+++ mission/src/store/modules/cart.js
@@ -18,7 +18,7 @@ const cart = {
         quantity: 1,
         name: '핏이 좋은 수트',
         image: 'https://projectlion-vue.s3.ap-northeast-2.amazonaws.com/items/suit-1.png',
-        price: 1000,
+        price: 198000,
         original_price: 298000,
         description: '아주 잘 맞는 수트',
       },
@@ -27,6 +27,14 @@ const cart = {
   getters: {
     totalPrice: (state) => state.items
       .reduce((acc, cur) => (cur.is_check ? acc + cur.price * cur.quantity : acc), 0),
+    summary: (state) => state.items
+      .filter((item) => item.is_check)
+      .map((item) => ({
+        product_no: item.product_no,
+        name: item.name,
+        quantity: `${item.quantity}개`,
+        price: `${(item.price * item.quantity).toLocaleString('ko-KR')}원`,
+      })),
   },
   mutations: {
     changeQuantity: (state, { index, quantity }) => {
