ef1b535a1da4ebf79e40de0d68b5ba0e69b13c97
한광재 <hkj4900@pusan.ac.kr>
2022-02-11 11:04:20 +0900

    Refactor: 사용하지 않는 선언 삭제

--- mission/src/store/index.js
+++ mission/src/store/index.js
@@ -2,12 +2,6 @@ import { createStore } from 'vuex';
 import cart from './modules/cart';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
     cart,
   },
