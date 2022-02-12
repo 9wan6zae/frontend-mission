4f2d0d35427781454f379a0aa69ab45e2b8d09c9
한광재 <hkj4900@pusan.ac.kr>
2022-02-13 08:41:15 +0900

    Feat: 라우팅 가드 추가

--- mission/src/router/routes/complete.js
+++ mission/src/router/routes/complete.js
@@ -1,7 +1,9 @@
 import CompletePage from '@/views/Complete.vue';
+import onlyIsChecked from '../routeGuards/onlyIsChecked';
 
 export default {
   path: '/complete',
   name: 'Complete',
   component: CompletePage,
+  beforeEnter: onlyIsChecked,
 };
--- mission/src/router/routes/order.js
+++ mission/src/router/routes/order.js
@@ -1,7 +1,9 @@
 import OrderPage from '@/views/Order.vue';
+import onlyIsChecked from '../routeGuards/onlyIsChecked';
 
 export default {
   path: '/order',
   name: 'Order',
   component: OrderPage,
+  beforeEnter: onlyIsChecked,
 };
