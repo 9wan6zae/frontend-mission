78b022e9dfaea605213f69eae6d87f1ad922e95f
한광재 <hkj4900@pusan.ac.kr>
2022-02-13 09:32:04 +0900

    Refactor: getCartItems로 아이템 받아오기, isAllCheck 변경

--- mission/src/views/Cart.vue
+++ mission/src/views/Cart.vue
@@ -8,13 +8,13 @@
         data-test="all-checkbox"
         :class="iconClass"
         :icon="['fas', 'check-square']"
-        @click="setCheckAll(isAllCheck)"
+        @click="setCheckAll"
       />
       <p>전체선택</p>
     </section>
     <section class="pa0-20 item-wrapper" v-if="!this.isEmptyCartItems">
       <cart-item
-        v-for="(item, index) in cart_items"
+        v-for="(item, index) in getCartItems"
         :key="item.product_no"
         v-bind="item"
         :index="index"
@@ -37,7 +37,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import message from '@/data/message';
 import Layout from '../components/Layouts/Layout.vue';
 import CartItem from '../components/Cart/CartItem.vue';
@@ -51,20 +51,9 @@ export default {
     FloatingActionBtn,
   },
   computed: {
-    ...mapState('cart', ['cart_items']),
-    ...mapGetters('cart', ['getTotalPrice', 'isEmptyCartItems']),
+    ...mapGetters('cart', ['getCartItems', 'getTotalPrice', 'isEmptyCartItems', 'isAllCheck']),
     iconClass() {
       return `checkbox ${this.isAllCheck ? 'checked' : 'unchecked'}`;
-    },
-    isAllCheck() {
-      if (!this.cart_items.length) return false;
-      for (let i = 0; i < this.cart_items.length; i += 1) {
-        const item = this.cart_items[i];
-        if (!item.is_check) {
-          return false;
-        }
-      }
-      return true;
     },
     btnText() {
       return `${this.getTotalPrice.toLocaleString('ko-kr')}원 구매`;
